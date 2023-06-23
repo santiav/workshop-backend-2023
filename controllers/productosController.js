f816541b8ff05f42fb22d121ea0b820e2e2d4f92
Santiago Acosta Verrier <santiago.acosta@bue.edu.ar>
2023-06-23 17:56:03 -0300

    Controladores creados

---
+++ controllers/productosController.js
@@ -0,0 +1,40 @@
+
+
+
+const obtenerProductos = (req, res) => {
+    res.send("Obteniendo el listado de productos")
+}
+
+const obtenerProductoPorId = (req, res) => { //  "/productos/:id"
+    const id = req.params.id
+    res.send("Obtener un producto por su ID " + id)
+}
+
+const editarProducto = (req, res) => {
+    const id = req.params.id
+    res.send("Enviando datos a la vista para prellenar formulario")
+}
+
+const actualizarProducto = (req, res) => {
+    const id = req.params.id
+    res.send("Editando el producto con ID " + id)
+} 
+
+const borrarProducto = (req, res) => {
+    const id = req.params.id
+    res.send("Borrando el producto con ID " + id)
+}
+
+const agregarProducto = (req, res) => {
+    res.send("Agregando producto a la DB")
+}
+
+module.exports = {
+    obtenerProductos,
+    obtenerProductoPorId,
+    editarProducto,
+    actualizarProducto,
+    borrarProducto,
+    agregarProducto
+}
+
--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -1,38 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { 
+    obtenerProductos,
+    obtenerProductoPorId, 
+    actualizarProducto,
+    editarProducto, 
+    borrarProducto,
+    agregarProducto 
+} = require("../controllers/productosController")
 
 // ver todos los productos "/productos"
-router.get("/", (req, res) => {
-    res.send("Obteniendo el listado de productos")
-})
+router.get("/", obtenerProductos)
 
 // Obtener un producto por su ID (ej vista de producto detallado)
-router.get("/:id", (req, res) => { //  "/productos/:id"
-    const id = req.params.id
-    res.send("Obtener un producto por su ID " + id)
-})
+router.get("/:id", obtenerProductoPorId)
 
 // Editar un producto (ej formulario prellenado para editar producto)
-router.get("/editar/:id", (req, res) => {
-    const id = req.params.id
-    res.send("Enviando datos a la vista para prellenar formulario")
-})
+router.get("/editar/:id", editarProducto)
 
 // Editar un producto enviando por formulario
-router.put("/:id", (req, res) => {
-    const id = req.params.id
-    res.send("Editando el producto con ID " + id)
-})
+router.put("/:id", actualizarProducto)
 
 // Borrar un producto
-router.delete("/:id", (req, res) => {
-    const id = req.params.id
-    res.send("Borrando el producto con ID " + id)
-})
+router.delete("/:id", borrarProducto)
 
 // Agregar un producto
-router.post("/", (req, res) => {
-    res.send("Agregando producto a la DB")
-})
+router.post("/", agregarProducto)
 
 module.exports = router;
